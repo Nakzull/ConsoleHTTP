9927b6148e431895b75815f7f8251756cc1a1a27
Nakzull <andr25f4@zbc.dk>
2023-02-16 13:02:02 +0100

    Exception fix

---
+++ ConsoleHTTP/RegexHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ConsoleHTTP
+{
+    internal class RegexHandler
+    {
+        public string StripHTML(string input)
+        {
+            var tempInput = Regex.Replace(input, "<.*?>", String.Empty);
+            return tempInput;
+        }
+    }
+}
